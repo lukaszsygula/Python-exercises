97e14c09aea20d492d031736e2eabd4714a77d47
lukasz-sygula <lukasz.sygula@onet.pl>
2023-08-02 20:46:48 +0200

    Function sphere volume exercise

---
+++ 22-Sphere volume.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Exercise: Write a function that computes the volume of a sphere given its radius
+
+def vol(rad):
+    return (4/3) * (3.14) * (rad**3)
+
+
+# In[3]:
+
+
+vol(2)
+
+
+# In[ ]:
+
+
+
+
